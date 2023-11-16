3f9e7f01edb6448b02cde4f2cf4bf48a9c4823ca
Hevelise - Liz <liz.souza.dias@gmail.com>
2023-11-16 16:12:08 -0300

    Subpastas categoria e Home.

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/views/Home";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Obrigada Deus!</Text>
+      <Home />
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/views/Home/index.jsx
@@ -0,0 +1,15 @@
+import {styles} from "./style";
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <View>
+      <Text style={styles.texto}> QUALQUER COISA </Text>
+
+
+    </View>
+  )
+}
+
+export default Home
